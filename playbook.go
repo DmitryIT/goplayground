c2bd13dab170169551605c1247d63afba186d454
DmitryIT <dmitryit.handy@gmail.com>
2021-10-30 17:07:39 +0200

    Develop (#2)
    * sec fix
    
    * add.go and tests added
    
    Co-authored-by: dmitry <dmitry>

--- playbook.go
+++ playbook.go
@@ -1,48 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
+
+	"github.com/DmitryIT/goplayground/operations"
 )
 
-type Counter struct {
-	total int
-}
-
-func (c Counter) String() {
-	fmt.Println("Counter = ", c.total)
-}
-
-func (c Counter) SetCounter(val int) error {
-	if val < 0 {
-		err := errors.New("total is negative")
-		return err
-	}
-	c.total = val
-	return nil
-}
-
-func (c *Counter) SetCounterPrt(val int) error {
-	if val < 0 {
-		err := errors.New("total is negative")
-		return err
-	}
-	c.total = val
-	return nil
-}
-
 func main() {
-	var cnt Counter
-	cnt.String()
-
-	if err := cnt.SetCounter(1); err != nil {
-		log.Fatalln(err)
-	}
-	cnt.String()
-
-	if err := cnt.SetCounterPrt(10); err != nil {
-		log.Fatalln(err)
-	}
-	cnt.String()
+	i, j := 1, 2
+	fmt.Printf("%d + %d = %d\n", i, j, operations.Add(i, j))
 }
